1cb093c4ce60b837a5b5b108c4a9374abada52a2
Krishna Shah <kshahusa@gmail.com>
2023-12-12 13:06:06 -0600

    Started postfix tests

--- tests/test_basic.py
+++
@@ -1,4 +0,0 @@
-from CalCoolUs.preprocess import ASTGraph
-
-def test_minimal():
-    assert True
---
+++ tests/test_postfix.py
@@ -0,0 +1,8 @@
+from CalCoolUs.preprocess import ShuntingYard
+
+myShunt = ShuntingYard()
+
+
+def test_postfix():
+    assert myShunt.getPostfix("x+2") == ["x", "2", "+"]
+    assert myShunt.getPostfix("2^-x") == ["2", "x", "-1", "*", "^"]
