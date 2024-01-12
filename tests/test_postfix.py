2be3453aa89cedf4d3a313a5871436fe56be94a3
Karhik Peravali <you@example.com>
2024-01-12 10:09:21 -0600

    Test Cases

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -8,4 +8,6 @@ def test_postfix():
     assert myShunt.getPostfix("2^-x") == ["2", "x", "-1", "*", "^"] or myShunt.getPostfix("2^-x") == ["2", "-1", "x", "*", "^"]
     assert myShunt.getPostfix("-(-x-1)-(-2-x)") == ["-1","-1", "x", "*", "1", "-", "*", "-2", "x", "-", "-"]
     assert myShunt.getPostfix("-(-x-1)^-(-2-x)") == ["-1","-1", "x", "*", "1", "-", "-1", "-2", "x", "-", "*", "^", "*"]
-    
+    #Failed
+    assert myShunt.getPostfix("(-(-x+1))^(-x+3)") == []
+    assert myShunt.getPostfix("-((x-3/2)/(x+1/2))^(-3/2)") == []
