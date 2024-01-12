d8024eaf0abed1b40e33a385c23e86cabdeefc3d
Nikson Thapa <niksonthapa11@gmail.com>
2024-01-12 09:59:43 -0600

    Added AndroidStudio code with a static text and idle button

---
+++ AndroidApp/app/src/main/java/com/example/testingapplication/ui/home/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.testingapplication.ui.home
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+//    implement lifecycle components later...
+}
