26e352a16c81260742bb8a74bd8df2ced725adf5
Karhik Peravali <you@example.com>
2024-01-19 09:59:30 -0600

    Test Cases for negative and exponential numbers

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -4,6 +4,8 @@ myShunt = ShuntingYard()
 
 
 def test_postfix():
-    assert myShunt.getPostfix("x+2") == ["x", "2", "+"]
-    assert myShunt.getPostfix("2^-x") == ["2", "x", "-1", "*", "^"]
-
+    assert myShunt.getPostfix("(2(4x+5))/3") == ["2", "4", "x", "5",  "+", "3", "/"]
+    assert myShunt.getPostfix("2^(-1*x)") == ["2", "-1", "x", "*", "^"]
+    assert myShunt.getPostfix("2(1+(1/x))") == ["2", "1", "1", "x", "/", "+"]
+    assert myShunt.getPostfix("2^x-5*x") == ["2", "x", "^", "5", "x", "*", "-" ]
+    assert myShunt.getPostfix("2^(1/x)") == ["2", "1", "x", "/", "^"]
