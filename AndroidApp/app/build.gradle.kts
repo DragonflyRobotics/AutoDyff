fea39119c77b6c760affcb06c2062e5c312c02fb
Nikson Thapa <niksonthapa11@gmail.com>
2024-03-01 09:18:48 -0600

    Changes to minimum API from 34 to 30

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.betterapp"
-        minSdk = 34
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
