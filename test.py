ad195e3fe507cc5f57500667c6a6d75bbd42ec9b
aryanjverma <111136645+aryanjverma@users.noreply.github.com>
2024-04-12 15:52:20 +0000

    contributes to #98 | creates new tokenizer using regex that inputs latex, works with basic coefs and operations

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 
 import networkx as nx
 from matplotlib import pyplot as plt
-
+import re
 from CalCoolUs.preprocess import ShuntingYard, OpType
 from CalCoolUs.ops.const import Const
 from CalCoolUs.error_types import *
 
 regex_latex = r"(\\sin)|(\\cos)|(\\tan)|(\\ln)|(\\log)|(\\arcsin)|(\\arccos)|(\\arctan)|(\\cot)|(\\csc)|(\\sec)|(\\sinh)|(\\cosh)|(\\tanh)|(\\arccsc)|(\\arcsec)|(\\arccot)|(\\sigmoid)|(\\sqrt)|((?<=\))-(?=.*))|({)|(})|(\\left\()|(\\right\))|(\d+\.\d+)|((?<!=\.)\d+(?!=\.))|((?<=\B)-\d+\.\d+)|((?<=\B)(?<!=\.)-\d+(?!=\.))|(x|(?<=\B)-x)|(e)|(\\pi)|(\^)|(\*)|(\\frac)|(\+)|(-\()|(-)" 
 
+string = r"2^{\left(x^2+5\right)}+\frac{4+x}{2\cdot\cos\left(x\right)}"
+
+myshunt = ShuntingYard()
+print(myshunt.tokenize_latex(r"2\cos\left(x\right)"))
