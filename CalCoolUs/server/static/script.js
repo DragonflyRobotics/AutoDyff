c96314b80e95f494983022bd06c21e05d66b44db
Brennan Coil <nerblioc@outlook.com>
2024-04-26 23:40:10 -0500

    Fully implemented mathquill into the website. Also fixed alignment of headings, added the favicon, and removed some dead code.

--- CalCoolUs/server/static/script.js
+++ CalCoolUs/server/static/script.js
@@ -54,7 +54,7 @@ function calc(stringify) {
         }
     };
 
-    xhttp.open("POST", "/numerical_engine/endpoint_latex");
+    xhttp.open("POST", "/vapor/brennan-coil/numerical_engine/endpoint_latex");
 
     xhttp.send(stringify); 
 }
